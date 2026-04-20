3721feabd79a6417afb64d1c34865a844654f95d
zaid <mz1944680@gmail.com>
2026-04-20 23:57:31 +0530

    feat: add authSlice in redux store

--- frontend/src/store/appStore.js
+++ frontend/src/store/appStore.js
@@ -2,14 +2,16 @@ import { configureStore } from "@reduxjs/toolkit"
 import notesSlice from '../features/notes/notesSlice'
 import foldersSlice from '../features/folders/foldersSlice'
 import notesContentSlice from "../features/notes/notesContentSlice";
-
+import authSlice from "./authSlice";
 
 const appStore=configureStore({
   reducer:{
     Notes:notesSlice.reducer,
     Folders:foldersSlice.reducer,
-    NotesContent:notesContentSlice.reducer
-  }
+    NotesContent:notesContentSlice.reducer,
+    Auth:authSlice.reducer,
+  },
 })
 
 export default appStore;
+
