456418a85d2aa830b4f319801e682a9552e87400
zaid <mz1944680@gmail.com>
2026-04-29 23:21:54 +0530

    feat: update api url and routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,14 +8,15 @@ import { Provider } from "react-redux";
 import LandingPage from "./pages/landinPage.jsx";
 import Editr from "./features/notes/editor/editor.jsx";
 import { AuthPage } from "./pages/signup-login.jsx";
-
+import ContentLayout from "./features/layout/contentLayout.jsx";
+const API_URL = import.meta.env.VITE_API_URL;
 const router = createBrowserRouter([
   { path: "/", element: <LandingPage /> },
   {
     path: "/home",
     element: <App />,
     loader: async () => {
-      const res = await fetch("http://localhost:8000/api/v1/users/workspace", {
+      const res = await fetch(`${API_URL}/api/v1/users/workspace`, {
         method: "GET",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
@@ -27,7 +28,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "notes/:id",
-        element: <Editr/>,
+        element: <ContentLayout />,
       },
     ],
   },
@@ -42,9 +43,7 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")).render(
-  //  <StrictMode>
   <Provider store={appStore}>
     <RouterProvider router={router} />
   </Provider>,
-  //  </StrictMode>
 );
