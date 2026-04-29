f86f1f0ea53f11deade5b643b7813c046d9aea53
zaid <mz1944680@gmail.com>
2026-04-30 00:21:58 +0530

    feat: add view state to toggle note and card view and add empty page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -5,32 +5,61 @@ import LeftSidebar from "../features/navigation/sidebar/leftSidebar";
 import ContextMenu from "../features/navigation/sidebar/contextMenu";
 import { UiController } from "../store/uiController";
 import { useDispatch, useSelector } from "react-redux";
-import { Outlet, useLoaderData } from "react-router-dom";
+import {
+  Outlet,
+  useLoaderData,
+  useNavigate,
+  useOutlet,
+} from "react-router-dom";
 import { hydrateApp } from "../store/appActions";
+import RightSidebar from "../features/flashcards/rightSidebar";
+import { setLoggedIn } from "../store/authSlice";
+import EmptyState from "./emptyPage";
 export const sidebarContext = createContext({});
+export const viewContext = createContext({});
 
 function App() {
+  const outlet = useOutlet();
+  const navigate = useNavigate();
   const data = useLoaderData();
   const Notes = useSelector((state) => state.Notes);
   const Folders = useSelector((state) => state.Folders);
   const NotesContent = useSelector((state) => state.NotesContent);
-
-  console.log("this is redux store data", Notes, Folders, NotesContent,"loader data",data);
+  const Flashcards = useSelector((state) => state.Flashcards);
+  console.log(
+    "this is redux store data",
+    Notes,
+    Folders,
+    NotesContent,
+    Flashcards,
+    "loader data",
+    data,
+  );
   console.log("App comp rendered ");
   const [ContextMenuPos, setContextMenuPos] = useState({});
   const [Rename, setRename] = useState(null);
   const [ShowContextMenu, setShowContextMenu] = useState(null);
-  const [ExpandLeftbar, setExpandLeftbar] = useState(null);
+  const [ExpandLeftbar, setExpandLeftbar] = useState(false);
   const [ExpandRightbar, setExpandRightbar] = useState(false);
+  const [view, setView] = useState("note");
   const [Active, setActive] = useState("r");
   const dispatch = useDispatch();
   const auth = useSelector((state) => state.Auth);
+  console.log("rightbar in home", ExpandRightbar);
 
   useEffect(() => {
     if (!data) return;
     dispatch(hydrateApp(data.payload));
   }, [data, dispatch]);
-  
+
+  useEffect(() => {}, []);
+
+  useEffect(() => {
+    if (!auth.isLoggedIn) {
+      navigate("../login");
+    }
+  }, [auth.isLoggedIn]);
+
   useEffect(() => {
     const handler = (e) => UiController.handler(e);
     document.addEventListener("mousedown", handler);
@@ -42,6 +71,8 @@ function App() {
       {
         <div className="h-screen flex flex-col overflow-hidden">
           <Header
+            view={view}
+            setView={setView}
             ExpandLeftbar={ExpandLeftbar}
             ExpandRightbar={ExpandRightbar}
             setExpandLeftbar={setExpandLeftbar}
@@ -61,11 +92,21 @@ function App() {
                 setRename,
               }}
             >
-              <LeftSidebar ExpandLeftbar={ExpandLeftbar} />
-              <ContextMenu />
+              <div className="flex-1 flex overflow-hidden">
+                <LeftSidebar ExpandLeftbar={ExpandLeftbar} />
 
-              <div className="flex-1 min-h-0 overflow-y-auto">
-                <Outlet />
+                <div className="flex-1 min-w-0 overflow-y-auto">
+                  <viewContext.Provider value={{ view }}>
+                    {outlet ? (
+                      <Outlet />
+                    ) : (
+                      <EmptyState setExpandLeftbar={setExpandLeftbar} />
+                    )}
+                  </viewContext.Provider>
+                </div>
+
+                <RightSidebar ExpandRightbar={ExpandRightbar} />
+                <ContextMenu />
               </div>
             </sidebarContext.Provider>
           </div>
