054d6b25ffc89a609d6e7893d852b8ded6c862d8
zaid <mz1944680@gmail.com>
2026-05-09 23:49:13 +0530

    feat: implement apiFetch wrapper for 401 error

---
+++ frontend/src/commons/apifetch.js
@@ -0,0 +1,51 @@
+import { useDispatch } from "react-redux";
+import { setLoggedIn } from "../store/authSlice";
+//import appStore from "../store/appStore";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const apiFetch = async (url, options = {}) => {
+  let response = await fetch(url, {
+    credentials: "include",
+
+    headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+    },
+
+    ...options,
+
+    body: options.body ? JSON.stringify(options.body) : undefined,
+  });
+
+  if (response.status === 401) {
+    const refreshRes = await fetch(`${API_URL}/api/v1/refresh`, {
+      method: "POST",
+      credentials: "include",
+    });
+
+    if (refreshRes.ok) {
+    //appStore.dispatch(setLoggedIn(true));
+      response = await fetch(url, {
+        credentials: "include",
+
+        headers: {
+          "Content-Type": "application/json",
+          ...(options.headers || {}),
+        },
+
+        ...options,
+
+        body: options.body ? JSON.stringify(options.body) : undefined,
+      });
+    } else {
+    //appStore.dispatch(setLoggedIn(false));
+    console.log('failed req in apifetch');
+    
+      return;
+    }
+  }
+  console.log('apifetch response return', response);
+  
+  return response;
+};
