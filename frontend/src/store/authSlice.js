08bee7fd5d636287920aa270056a95826c9b145a
zaid <mz1944680@gmail.com>
2026-05-12 00:17:49 +0530

    feat: add new field in authSlice and respective reducer

--- frontend/src/store/authSlice.js
+++ frontend/src/store/authSlice.js
@@ -1,22 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
-  name:'isLoggedIn',
-  initialState:{
-    user:null,
-    isLoggedIn:false
+  name: "isLoggedIn",
+  initialState: {
+    isAuthChecked: null,
+    user: null,
+    isLoggedIn: false,
   },
-  reducers:{
-    setLoggedIn:(state, action)=>{
-      state.user = action.payload,
-      state.isLoggedIn = true
+  reducers: {
+    setLoggedIn: (state, action) => {
+      console.log("authslice setloggedin", action.payload);
+      state.isAuthChecked = true;
+      state.user = action.payload;
+      state.isLoggedIn = true;
     },
-    setLoggedOut:(state, action)=>{
-      state.user = null,
-      state.isLoggedIn = false
-    }
-  }
-}) 
+    setLoggedOut: (state, action) => {
+      state.user = null;
+      state.isLoggedIn = false;
+    },
+    toggleIsAuthChecked: (state, action) => {
+      state.isAuthChecked = !state.isAuthChecked;
+    },
+  },
+});
 
-export const {setLoggedIn,setLoggedOut} = authSlice.actions
-export default authSlice
+export const { setLoggedIn, setLoggedOut } = authSlice.actions;
+export default authSlice;
