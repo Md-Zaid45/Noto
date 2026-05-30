6ffa94e658bb8f5d48ebfeb27805458b727767ef
zaid <mz1944680@gmail.com>
2026-05-30 23:52:40 +0530

    feat: add controller for dashboard data

--- backend/src/controllers/flashcard.controller.js
+++ backend/src/controllers/flashcard.controller.js
@@ -50,13 +50,11 @@ export const reviewUpdate = async (req, res, next) => {
     const card = await Flashcard.findById(id);
     if (!card) throw new ApiError(404, "Flashcard not found");
     const updatedCard = await card.reviewUpdate(score);
-    return res
-      .status(200)
-      .json({
-        success: true,
-        message: "updated review successfully",
-        payload: { flashcard: updatedCard },
-      });
+    return res.status(200).json({
+      success: true,
+      message: "updated review successfully",
+      payload: { flashcard: updatedCard },
+    });
   } catch (error) {
     return next;
   }
@@ -80,5 +78,83 @@ export const deleteFlashcards = async (req, res, next) => {
     });
   } catch (error) {
     next(error);
+  }
+};
+
+export const getFlashcardActivity = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+    const start = new Date();
+    start.setHours(0, 0, 0, 0);
+    const end = new Date();
+    end.setHours(23, 59, 59, 999);
+    const futureEnd = new Date();
+    futureEnd.setDate(futureEnd.getDate() + 30);
+
+    const [
+      totalCards,
+      activeCards,
+      dueCards,
+      reviewedCards,
+      masteredCards,
+      learningCards,
+      newCards,
+    ] = await Promise.all([
+      Flashcard.countDocumetns({ _id: userId }),
+      Flashcard.countDocuments({
+        userId,
+        revisionMark: true,
+      }),
+      Flashcard.countDocuments({
+        userId,
+        revisionMark: true,
+        nextReview: new Date(),
+      }),
+
+      Flashcard.countDocuments({
+        userId,
+        revisionMark: true,
+        updatedAt: {
+          $gte: start,
+          $lte: end,
+        },
+      }),
+      Flashcard.countDocuments({
+        userId,
+        revisionMark: true,
+        interval: { $gte: 30 },
+      }),
+      Flashcard.countDocuments({
+        userId,
+        revisionMark: true,
+        repetitions: { $gte: 3 },
+      }),
+      Flashcard.countDocuments({
+        userId,
+        revisionMark: true,
+        repetitions: { $gte: 0, $lte: 3 },
+      }),
+    ]);
+
+    const futureCards = await Flashcard.aggregate([
+      {$match:{userId, revisionMark: true, nextReview: { $gt: end, $lte: futureEnd }}},
+      {$group:{nextReview: { $dateToString: { format: "%Y-%m-%d", date: "$nextReview" } }, count: { $sum: 1 }}},
+      {$sort: { nextReview: 1 }}
+    ])
+    return res.status(200).json({
+      success: true,
+      payload: {
+        totalCards,
+        activeCards,
+        dueCards,
+        reviewedCards,
+        masteredCards,
+        learningCards,
+        newCards,
+        futureCards,
+      },
+    });
+  } catch (error) {
+    return next(error);
   }
 };
