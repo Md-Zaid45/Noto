df61d6dd32a50c5bf41c48ef3a8da725567c0d9a
zaid <mz1944680@gmail.com>
2026-06-16 23:59:47 +0530

    fix: recent.tabs throwing error if recents is empty

--- backend/src/controllers/note.controller.js
+++ backend/src/controllers/note.controller.js
@@ -108,11 +108,11 @@ export const getNoteCount = async (req, res, next) => {
 export const recentNotes = async (req, res, next) => {
   try {
     const recents = req.body;
-    const ids = recents.tabs.map(tab => tab.id);
+    const ids = recents?.tabs.map(tab => tab.id);
     const recentNotes = await Note.find({ userId: req.user._id, _id: { $in: ids } }).select('_id name content type ')
     
     console.log("recent notes", recentNotes, req.notes, req.folders);
-    if(recents.tabs.length == 0 || recentNotes.length == 0){
+    if(recents?.tabs.length == 0 || recentNotes?.length == 0){
       return res.status(200).json({
         payload: {
           folders: req.folders,
